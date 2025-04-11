81e3941d3e83aa927f2fb84bd176819dd6aee1f0
Joseph-PC <Joseph-PC@DESKTOP-8AP4S74>
2025-04-11 17:07:59 -0500

    Creacion de entidades y del dataContext

--- CulturaVivaTours.Shared/Entites/Turista.cs
+++ CulturaVivaTours.Shared/Entites/Turista.cs
@@ -9,32 +9,40 @@ namespace CulturaVivaTours.Shared.Entites
 {
     public class Turista
     {
-        [Display(Name = "# Documento turista")]
-        [Required]
-        [MaxLength(15)]
+        
         public string id { get; set; }
+ 
 
-        [Display(Name = "Nombre turista")]
-        [Required]
-        [MaxLength(75)]
+        [Display(Name = "Nombre turista")] //Pone una Descripcion del campo
+        [Required] //Hace que sea obligatorio
+        [MaxLength(75, ErrorMessage = "Nombre Demasiado largo")]//Caracteres maximos y mensaje de error
         public string FullName { get; set; }
 
 
         [Display(Name = "Nacionalidad turista")]
         [Required]
-        [MaxLength(40)]
+        [MaxLength(40,ErrorMessage = "Nacionalidad Demasiado largo")]
         public string Nacionality { get; set; }
 
         [Display(Name = "Idioma turista")]
         [Required]
-        [MaxLength(40)]
-        public string Lenguage { get; set; }
+        [MaxLength(40, ErrorMessage = "Idioma Demasiado largo")]
+        public string Language { get; set; }
 
 
-        [Display(Name = "Comida Restringida turista")]
+        [Display(Name = "Comida Restringida turista (si aplica)")]
+        [MaxLength(100, ErrorMessage = "Comida Restringida Demasiado largo")]
+        public string RestringFood { get; set; }
+
+
+        [Display(Name = "Idioma turista")]
         [Required]
-        [MaxLength(50)]
-        public int RestringFood { get; set; }
+        [MaxLength(40, ErrorMessage = "Idioma Demasiado largo" )]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime Disponibility { get; set; }
+
+
+
 
 
 
