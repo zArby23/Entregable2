4fe71e1991f7d91a65d4138d46b4cf9ba45bdc20
Joseph-PC <Joseph-PC@DESKTOP-8AP4S74>
2025-04-11 15:48:00 -0500

    Creacion de clase turista en el shared

---
+++ CulturaVivaTours.Shared/Entites/Turista.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CulturaVivaTours.Shared.Entites
+{
+    public class Turista
+    {
+        [Display(Name = "# Documento turista")]
+        [Required]
+        [MaxLength(15)]
+        public string id { get; set; }
+
+        [Display(Name = "Nombre turista")]
+        [Required]
+        [MaxLength(75)]
+        public string FullName { get; set; }
+
+
+        [Display(Name = "Nacionalidad turista")]
+        [Required]
+        [MaxLength(40)]
+        public string Nacionality { get; set; }
+
+        [Display(Name = "Idioma turista")]
+        [Required]
+        [MaxLength(40)]
+        public string Lenguage { get; set; }
+
+
+        [Display(Name = "Comida Restringida turista")]
+        [Required]
+        [MaxLength(50)]
+        public int RestringFood { get; set; }
+
+
+
+
+    }
+}
