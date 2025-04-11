cd36919a101fe6e83f4c010ccbb20f9eba6980b4
Juan Montoya (Arby) <lic.jdavid23@gmail.com>
2025-04-11 17:57:52 -0500

    Entidad "Guia" creada con sus relaciones.

---
+++ CulturaVivaTours.Shared/Entities/RutaGuia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CulturaVivaTours.Shared.Entities
+{
+    public class RutaGuia
+    {
+        [JsonIgnore]
+        public Ruta Ruta { get; set; }
+        public int RutaId { get; set; }
+
+        [JsonIgnore]
+        public Guia Guia { get; set; }
+
+    }
+}
