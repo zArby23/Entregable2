3946da33c031a1a7ceb477c8e2653470640c400f
SEBASTIAN RAMIREZ MU�OZ <sebastianramirez1112697@correo.itm.edu.co>
2025-04-12 16:36:36 -0500

    Crear entidad entidad Proveedor, PuntoInteres y sus relaciones

---
+++ CulturaVivaTours.Shared/Entities/Calificacion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CulturaVivaTours.Shared.Entites
+{
+    public class Calificacion
+    {
+      
+        public string id { get; set; }//Primary key
+
+
+        public string FullName { get; set; }
+
+
+        [Display(Name = "Puntuacion del turista")]
+        [Required]
+        [MaxLength(40, ErrorMessage = "Nacionalidad Demasiado largo")]
+        public string Puntuation { get; set; }
+
+        [Display(Name = "Comentario del turista")]
+        [Required]
+        [MaxLength(40, ErrorMessage = "Idioma Demasiado largo")]
+        public string Coment { get; set; }
+
+        [JsonIgnore]
+        public PuntoInteres PuntoInteres { get; set; }
+
+        [JsonIgnore]
+        public Turista Turista { get; set; }
+
+ 
+
+
+
+
+    }
+}
--- CulturaVivaTours.Shared/Entities/PuntoInteres.cs
+++ CulturaVivaTours.Shared/Entities/PuntoInteres.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace CulturaVivaTours.Shared.Entities
+namespace CulturaVivaTours.Shared.Entites
 {
-    public internal class PuntoInteres
+    public class PuntoInteres
     {
+      
+        public int id { get; set; }//Primary key
+
+        [Display(Name = "Descripcion del Punto de Interes")]
+        [Required]
+        [MaxLength(150, ErrorMessage = "Descripcion demasiada larga")]
+        public string Descripcion { get; set; }
+
+
+        [Display(Name = "Lugar de punto de interes")]
+        [Required]
+        [MaxLength(50, ErrorMessage = "Mucho texto")]
+        public string Ubicacion { get; set; }
+
+
+        [Display(Name = "Descripcion de tipo de actividad")]
+        [Required]
+        [MaxLength(50, ErrorMessage = "Mucho texto")]
+        public string TipoActividad { get; set; }
+
+
+        [Display(Name = "Hora de llegada al punto de interes")]
+        [Required]
+        [MaxLength(50, ErrorMessage = "Mucho texto")]
+        [DisplayFormat(DataFormatString = "{0:hh:mm}")]
+        public DateTime HorarioEstimado { get; set; }
+
+
+        [JsonIgnore]
+        public Proveedor Proveedor { get; set; }
+
+        [JsonIgnore]
+        public ICollection<Calificacion> Calificaciones { get; set; }
+
+        [JsonIgnore]
+        public Ruta Ruta { get; set; }
     }
 }
