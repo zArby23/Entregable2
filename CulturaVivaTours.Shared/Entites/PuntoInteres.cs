2583a489098dd238020803446c9e508ddcc6973c
Juan Montoya (Arby) <lic.jdavid23@gmail.com>
2025-04-12 18:46:13 -0500

    Revert "Borrada la carpeta "Entites""
    This reverts commit 66f653d31c75201ac2d805074df94b0c8eef5a2a.

---
+++ CulturaVivaTours.Shared/Entites/PuntoInteres.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CulturaVivaTours.Shared.Entites
+{
+    public class PuntoInteres
+    {
+      
+        public int id { get; set; }//Primary key
+
+
+
+
+
+
+        //Conexion con la tabla calificacion
+        [JsonIgnore]
+        public ICollection<Calificacion> Calificacions{ get; set; }
+
+    }
+}
