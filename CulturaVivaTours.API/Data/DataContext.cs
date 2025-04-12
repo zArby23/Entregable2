0f9adfe87e514ee64850be8d388c2ff3686f73c4
Joseph-PC <Joseph-PC@DESKTOP-8AP4S74>
2025-04-12 13:50:04 -0500

    Creacion del dataContext con las entidades Calificacion y Turista

--- CulturaVivaTours.API/Data/DataContext.cs
+++ CulturaVivaTours.API/Data/DataContext.cs
@@ -1,11 +1,31 @@
-﻿using CulturaVivaTours.Shared.Entities;
-using Microsoft.EntityFrameworkCore;
-
+﻿using Microsoft.EntityFrameworkCore;
+using CulturaVivaTours.Shared.Entites;
 namespace CulturaVivaTours.API.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<Turista> Turistas { get; set; }
+
+        public DbSet<Calificacion> calificacions { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+
+
 
     }
 }
+
+ 
