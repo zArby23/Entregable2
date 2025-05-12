11d9a8ef793970109587033827575a9eb372ed8f
Juan Montoya (Arby) <lic.jdavid23@gmail.com>
2025-05-12 16:41:42 -0500

    Refactor IRepository for async method naming
    Updated IRepository interface to use async naming conventions for Get, Post, and Put methods. Added DeleteAsync method for DELETE requests. Modified method signatures to support generic response types for PostAsync and PutAsync. Updated Repository class implementation to align with these changes, enhancing clarity and consistency in asynchronous operations.

--- CulturaVivaTours.WEB/Repositories/Repository.cs
+++ CulturaVivaTours.WEB/Repositories/Repository.cs
@@ -17,7 +17,7 @@ namespace CulturaVivaTours.WEB.Repositories
             _httpClient = httpClient;
         }
 
-        public async Task<HttpResponseWrapper<T>> Get<T>(string url)
+        public async Task<HttpResponseWrapper<T>> GetAsync<T>(string url)//200
         {
             var responseHttp = await _httpClient.GetAsync(url);
             if (responseHttp.IsSuccessStatusCode)
@@ -29,7 +29,7 @@ namespace CulturaVivaTours.WEB.Repositories
             return new HttpResponseWrapper<T>(default, true, responseHttp);
         }
 
-        public async Task<HttpResponseWrapper<object>> Post<T>(string url, T model)
+        public async Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model)
         {
             var mesageJSON = JsonSerializer.Serialize(model);
             var messageContet = new StringContent(mesageJSON, Encoding.UTF8, "application/json");
@@ -37,7 +37,7 @@ namespace CulturaVivaTours.WEB.Repositories
             return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
         }
 
-        public async Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model)
+        public async Task<HttpResponseWrapper<TResponse>> PostAsync<T, TResponse>(string url, T model)//200
         {
             var messageJSON = JsonSerializer.Serialize(model);
             var messageContet = new StringContent(messageJSON, Encoding.UTF8, "application/json");
@@ -55,6 +55,36 @@ namespace CulturaVivaTours.WEB.Repositories
             var respuestaString = await httpResponse.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<T>(respuestaString, jsonSerializerOptions)!;
         }
-    }
 
+        public async Task<HttpResponseWrapper<object>> PutAsync<T>(string url, T model)
+        {
+            var messageJSON = JsonSerializer.Serialize(model);
+            var messageContet = new StringContent(messageJSON, Encoding.UTF8, "application/json");
+            var responseHttp = await _httpClient.PutAsync(url, messageContet);
+            return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
+
+        }
+
+        public async Task<HttpResponseWrapper<TResponse>> PutAsync<T, TResponse>(string url, T model)//200
+        {
+            var messageJSON = JsonSerializer.Serialize(model);
+            var messageContet = new StringContent(messageJSON, Encoding.UTF8, "application/json");
+            var responseHttp = await _httpClient.PutAsync(url, messageContet);
+            if (responseHttp.IsSuccessStatusCode)
+            {
+                var response = await UnserializeAnswer<TResponse>(responseHttp, _jsonDefaultOptions);
+                return new HttpResponseWrapper<TResponse>(response, false, responseHttp);
+            }
+            return new HttpResponseWrapper<TResponse>(default, !responseHttp.IsSuccessStatusCode, responseHttp);
+        }
+
+
+
+        public async Task<HttpResponseWrapper<object>> DeleteAsync(string url)
+        {
+            var responseHttp = await _httpClient.DeleteAsync(url);
+
+            return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);//204
+        }
+    }
 }
