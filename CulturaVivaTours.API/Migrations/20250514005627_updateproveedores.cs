a4d6da26896048c2e481cda9009541f448a2da60
Joseph-PC <Joseph-PC@DESKTOP-8AP4S74>
2025-05-13 20:00:12 -0500

    Udptade de la base de datos

---
+++ CulturaVivaTours.API/Migrations/20250514005627_updateproveedores.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CulturaVivaTours.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateproveedores : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Disponible",
+                table: "Proveedores");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Proveedores",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Proveedores",
+                newName: "id");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Disponible",
+                table: "Proveedores",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
